c993d7245eb6abf542cd916aa1efd44b30719097
Gaurav7867 <112535303+Gaurav7867@users.noreply.github.com>
2024-02-06 23:30:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 //your JS code here. If required.
+const gridContainer=document.querySelector(".grid");
+for(let i=1;i<=9;i++){
+const gridItem=document.createElement("div");
+gridItem.id=i;
+gridItem.innerText=i;
+gridItem.className="grid-item"
+gridContainer.appendChild(gridItem);
+}
+
+
+let previousSelectedElement=null;
+const form=document.querySelector("#form")
+form.addEventListener("submit",function(event){
+event.preventDefault();
+let cellid=form.inputid.value;
+let selectedcolor=form.colorid.value;
+let cell=document.getElementById(cellid);
+cell.style.backgroundColor=selectedcolor;
+if(previousSelectedElement){
+    previousSelectedElement.style.backgroundColor="white";
+}
+previousSelectedElement=cell;
+});
+
+
+
+
+document.addEventListener("contextmenu",(event)=>{
+// event.preventDefault();
+console.log("inside context");
+});
